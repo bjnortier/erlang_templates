cf5aa08b93a871b6f4bf6f32a9fef19b8aaff85f
Benjamin Nortier <bjnortier@gmail.com>
2010-11-08 14:37:01 +0000

    updated template

--- gen_server_template.erl
+++ gen_server_template.erl
@@ -1,8 +1,7 @@
 -module(gen_server_template).
--export([start_link/0, stop/0]).
 -behaviour(gen_server).
-
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([start_link/0, stop/0]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%                              Public API                                  %%%
